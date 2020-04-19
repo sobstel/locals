79288640d9231733a5d781da1b5535d887e8304b
Przemek Sobstel <przemek@sobstel.org>
2020-04-19 13:03:16 +0200

    Ignore purgecss for antd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from "next/app";
 
+/* purgecss ignore */
 import "antd/dist/antd.css";
+/* purgecss enable */
 import "../style.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
