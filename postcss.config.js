015c76e10cd91aea14dfc361f7f578fbab2394b4
Przemek Sobstel <przemek@sobstel.org>
2020-04-19 13:21:11 +0200

    Remove purgecss for now

--- postcss.config.js
+++ postcss.config.js
@@ -4,19 +4,6 @@ module.exports = {
     "tailwindcss",
     ...(process.env.NODE_ENV === `production`
       ? [
-          [
-            "@fullhuman/postcss-purgecss",
-            {
-              content: [
-                "./pages/**/*.tsx",
-                "./pages/**/*.jsx",
-                "./components/**/*.tsx",
-                "./components/**/*.jsx",
-              ],
-              defaultExtractor: (content) =>
-                content.match(/[A-Za-z0-9-_:/]+/g) || [],
-            },
-          ],
           "autoprefixer",
           "cssnano"
         ]
