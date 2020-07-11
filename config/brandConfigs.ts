62bcbf625cd4c911b591682a44df87157d3190d7
Przemek Sobstel <przemek@sobstel.org>
2020-07-11 15:10:39 +0200

    brand setup

---
+++ config/brandConfigs.ts
@@ -0,0 +1,12 @@
+export default [
+  {
+    id: "niebanalna",
+    name: "Niebanalna",
+    email: "przemek@sobstel.org",
+    address: false,
+    spreadsheet: {
+      id: "1cmIgHjj2sUlPy81U8Dr0FPCSXjux-LXqyOCHCmC2FjI",
+      range: "B36:C",
+    },
+  },
+];
--- config/defaultConfig.ts
+++ config/defaultConfig.ts
@@ -2,7 +2,7 @@ export default {
   id: "demo",
   name: "Demo",
   email: "przemek@sobstel.org",
-  address: false,
+  address: true,
   spreadsheet: {
     id: "10aITrOIuPLwEn0kh_drNMUzJR4i1MxwqKH06gZX_XTs",
     range: "B18:C",
